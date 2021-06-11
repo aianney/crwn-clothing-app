59be7b1897c8520497a22942b51d320d00253773
yannie4163 <73107567+yannie4163@users.noreply.github.com>
2021-06-11 18:43:25 +0800

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.comonent';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -69,7 +70,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,12 +11,15 @@ const config = {
     appId: "1:6426838990:web:d8b2f73a2a5194dde2cd9f"
   };
 
+  firebase.initializeApp(config);
+
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
     const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapShot = await userRef.get();
 
+    const snapShot = await userRef.get();
+   
     if(!snapShot.exists){
       const { displayName, email} = userAuth;
       const createdAt = new Date();
@@ -36,7 +39,38 @@ const config = {
     return userRef;
   };
 
-  firebase.initializeApp(config);
+  export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+  ) => {
+    const collectionRef = firestore.collection(collectionKey);
+  
+    const batch = firestore.batch();
+    objectsToAdd.forEach((obj) => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+    return await batch.commit();
+  };
+
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map((doc) => {
+      const { title, items } = doc.data();
+  
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items,
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  };
+
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
